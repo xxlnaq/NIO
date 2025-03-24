ddd51e843b484f11255d68ca9e5b984c2b80eb1a
xxl <1438340069@qq.com>
2025-03-24 10:53:57 +0800

    Updata Hanlder

--- src/main/java/com/example/Reactor/Handler.java
+++ src/main/java/com/example/Reactor/Handler.java
@@ -7,7 +7,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Handler implements Runnable{
-    private  static  final  ExecutorService Pool = Executors.newFixedThreadPool(10);
+    private static final ExecutorService POOL = Executors.newFixedThreadPool(10);
 
     private final SocketChannel channel;
 
@@ -18,13 +18,19 @@ public class Handler implements Runnable{
     @Override
     public void run() {
         try {
-            ByteBuffer buffer = ByteBuffer.allocate(128);
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
             channel.read(buffer);
             buffer.flip();
-            System.out.println("接收到客户端数据："+new String(buffer.array(), 0, buffer.remaining()));
-            channel.write(ByteBuffer.wrap("已收到！".getBytes()));
-        }catch (IOException e){
-            e.printStackTrace();
+            POOL.submit(() -> {
+                try {
+                    System.out.println("接收到客户端数据："+new String(buffer.array(), 0, buffer.remaining()));
+                    channel.write(ByteBuffer.wrap("已收到！".getBytes()));
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
