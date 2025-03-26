a4983a3e02214f680cd35e41ae94899bce8cbb76
xxl <1438340069@qq.com>
2025-03-26 13:30:05 +0800

    解决客户端断开连接后，服务器仍监听的方法

--- src/main/java/com/example/Reactor/Handler.java
+++ src/main/java/com/example/Reactor/Handler.java
@@ -19,7 +19,15 @@ public class Handler implements Runnable{
     public void run() {
         try {
             ByteBuffer buffer = ByteBuffer.allocate(1024);
-            channel.read(buffer);
+           if(channel.read(buffer) < 0) {
+               System.out.println("客户端已断开连接"+channel.getRemoteAddress());
+               channel.close();
+               return;//添加return后直接退出了run方法
+               /**
+                * 不会的，在Java中，return语句会立即从当前方法返回，终止方法的执行。
+                * 因此，如果return语句被执行了，那么它后面的代码（包括buffer.flip()和提交到线程池的任务）将不会被执行。
+                */
+           }
             buffer.flip();
             POOL.submit(() -> {
                 try {
